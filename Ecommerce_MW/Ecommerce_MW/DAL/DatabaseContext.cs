80cc317f34f6223b2b386007e62d92926708ebc4
CESAR AUGUSTO BARRIENTOS CORDOBA <cesarbarrientos298871@correo.itm.edu.co>
2023-03-25 19:02:19 -0500

    Creat a new web app

---
+++ Ecommerce_MW/Ecommerce_MW/DAL/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce_MW.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_MW.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
---
+++ Ecommerce_MW/Ecommerce_MW/DAL/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce_MW.DAL.Entities
+{
+    public class Entity
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        [Display(Name = "Fecha de creacion")]
+        public DateTime? CreatedDate { get; set; }
+
+        [Display(Name = "Fecha de modificaion")]
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
